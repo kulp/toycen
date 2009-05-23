3404ca0a8a20b085c9625eb824c9cb0fbff8220f
Darren Kulp <darren@kulp.ch>
2009-05-22 20:55:48 -0500

    Create _error() macro.
    Eventually this should probably be renamed, and
    should provide context about where it was called
    (in terms of user input, not necessarily in terms
    of the source line from which it was invoked).

--- debug.h
+++ debug.h
@@ -1,6 +1,8 @@
 #ifndef DEBUG_H_F828480A004155BCAD874C168AB38287
 #define DEBUG_H_F828480A004155BCAD874C168AB38287
 
+/// @todo use redirectable output streams
+
 #if DEBUG
 #include <stdio.h>
 #define _debug(n,...) \
@@ -8,10 +10,17 @@
 		if (n <= DEBUG) { \
 			fprintf(stderr, __VA_ARGS__); \
 			fprintf(stderr, "\n"); \
-		} while (0)
+		} \
+	while (0)
 #else
 #define _debug(...)
 #endif
+
+#define _error(...) \
+    do { \
+		fprintf(stderr, __VA_ARGS__); \
+		fprintf(stderr, "\n"); \
+	} while (0)
 
 #endif
 
