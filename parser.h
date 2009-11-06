3cfe92109f90b0ff4151666b23d04885e9fa48a3
Darren Kulp <darren@kulp.ch>
2009-11-06 08:04:49 -0600

    Include ast.h

--- parser.h
+++ parser.h
@@ -1,6 +1,7 @@
 #ifndef PARSER_H_D0BAB84A620FF56F821864BE64C127E0
 #define PARSER_H_D0BAB84A620FF56F821864BE64C127E0
 
+#include "ast.h"
 #include "debug.h"
 #include "parser_internal.h"
 #include "hash_table.h"
@@ -16,3 +17,4 @@ void parser_teardown(parser_state_t *ps);
 #endif
 
 /* vi:set ts=4 sw=4 et syntax=c.doxygen: */
+
