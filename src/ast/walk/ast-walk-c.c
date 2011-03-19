1758aa2226322c6c1ac77635540a7b304f8bd982
Darren Kulp <darren@kulp.ch>
2011-03-18 22:21:47 -0500

    Working on C walker

---
+++ src/ast/walk/ast-walk-c.c
@@ -0,0 +1,168 @@
+#include "lexer.h"
+#include "pp_lexer.h"
+#include "parser.h"
+#include "hash_table.h"
+#include "ast-walk.h"
+#include "ast-ids-priv.h"
+#include "ast-formatters.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern int yyparse();
+
+int DEBUG_LEVEL = 2;
+FILE* DEBUG_FILE;
+
+static const char *flags_names[] = {
+    #define R_(X) [AST_WALK_##X] = #X,
+    R_(BEFORE_CHILDREN)
+    R_(AFTER_CHILDREN)
+    R_(BETWEEN_CHILDREN)
+    #undef R_
+};
+
+static const char *meta_names[] = {
+    #define R_(X) [META_IS_##X] = #X,
+    R_(INVALID)
+    R_(NODE)
+    R_(ID)
+    R_(CHOICE)
+    R_(PRIV)
+    R_(BASIC)
+    #undef R_
+};
+
+struct mydata {
+    int indent;
+};
+
+static int walk_cb(
+        int flags,
+        enum meta_type meta,
+        unsigned type,
+        void *data,
+        void *userdata,
+        struct ast_walk_ops *ops,
+        walkdata cookie
+    )
+{
+    struct mydata *c = userdata;
+
+    static const struct flag_rec {
+        char *prefix;
+        signed indent;
+    } flag_recs[8] = {
+        [AST_WALK_BEFORE_CHILDREN ] = { "{",  4 },
+        [AST_WALK_AFTER_CHILDREN  ] = { "}", -4 },
+    };
+
+    int botmask = (AST_WALK_BEFORE_CHILDREN |
+                   AST_WALK_BETWEEN_CHILDREN |
+                   AST_WALK_AFTER_CHILDREN);
+    bool before  = flags & AST_WALK_BEFORE_CHILDREN;
+    bool after   = flags & AST_WALK_AFTER_CHILDREN;
+    bool between = flags & AST_WALK_BETWEEN_CHILDREN;
+    bool once    = !(flags & botmask);
+    bool structp = false; // XXX
+
+    const char *name = NULL;
+    ops->get_name(cookie, &name);
+
+    const struct flag_rec *f = &flag_recs[flags & botmask];
+    assert(c->indent < 256);
+    char spaces[c->indent + 1];
+    memset(spaces, ' ', sizeof spaces);
+    spaces[c->indent] = 0;
+    fputs(spaces, stdout);
+    c->indent += f->indent;
+
+    if (name)
+        if (before || meta == META_IS_BASIC || meta == META_IS_ID)
+            printf(".%s = ", name);
+
+    // type name
+    const char *tname = NULL;
+
+    switch (meta) {
+        case META_IS_PRIV: {
+            const struct priv_rec *rec = &priv_recs[type];
+            tname = rec->name;
+            break;
+        }
+        case META_IS_NODE: {
+            const struct node_rec *rec = &node_recs[type];
+            tname = rec->name;
+            break;
+        }
+        case META_IS_BASIC: {
+            char buf[128];
+            int size = sizeof buf;
+            int result = fmt_call(meta, type, &size, buf, data);
+            printf("%s", buf);
+            break;
+        }
+        case META_IS_ID: {
+            char buf[128];
+            int size = sizeof buf;
+            int result = fmt_call(meta, type, &size, buf, data);
+            printf("%s", buf);
+            break;
+        }
+        case META_IS_CHOICE:
+            break;
+        default:
+            // TODO handle
+            abort();
+    }
+
+    if (!name && before)
+        printf("(struct %s)", tname);
+
+    if (after && structp)
+        printf("} ", tname);
+
+    if (f->prefix)
+        printf("%s", f->prefix);
+
+    if (before && structp)
+        printf(" &(struct %s){", tname);
+
+    if (after || once)
+        putchar(',');
+
+    puts("");
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int result;
+
+    DEBUG_FILE = stdout;
+
+    parser_state_t ps;
+
+    if (argc > 1)
+        switch_to_input_file(argv[1]);
+
+    lexer_setup();
+    parser_setup(&ps);
+    result = yyparse();
+
+    struct translation_unit *top = get_top_of_parse_result();
+
+    struct mydata data = { 0 };
+    ast_walk((struct node*)top, walk_cb, AST_WALK_BEFORE_CHILDREN |
+                                         AST_WALK_AFTER_CHILDREN, &data);
+
+    parser_teardown(&ps);
+    lexer_teardown();
+
+    return result;
+}
+
+/* vi:set ts=4 sw=4 et syntax=c.doxygen: */
