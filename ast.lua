eb96f764e8b3c6f70df056b979897dade405b596
Darren Kulp <darren@kulp.ch>
2011-07-14 16:26:58 -0500

    Making things harder to read (compactification)

--- ast.lua
+++ ast.lua
@@ -13,7 +13,7 @@ ffi.cdef[[
 ]]
 local libast = ffi.load("libast.so")
 
-local function decode_node_item(node_item,parent)
+local function decode_node_item(node_item, parent)
     local table = {
         [tonumber(ffi.cast("enum meta_type", "META_IS_NODE"  ))] = node_item.c.node,
         [tonumber(ffi.cast("enum meta_type", "META_IS_PRIV"  ))] = node_item.c.priv,
@@ -31,32 +31,34 @@ local function type_from_node_enum(x)
 end
 
 -- XXX hokey priv-detection (trailing underscore ? is this official ?)
-local function rec_from_tag(mytag)
-    local stem = (mytag:sub(-1) == "_") and "priv_type"  or "node_type"
-    return libast.node_recs[ffi.cast("enum "..stem, stem:upper()..'_'..mytag)]
+local function rec_from_tag(tag)
+    local stem = (tag:sub(-1) == "_") and "priv_type"  or "node_type"
+    return libast.node_recs[ffi.cast("enum "..stem, stem:upper()..'_'..tag)]
 end
 
 -- XXX hokey check for anonymous aggregate
-local function is_anonymous(mytag)
-    return not mytag:find("%d+")
-end
+local function is_anonymous(tag) return not tag:find("%d+") end
 
-local function doformat(userdata,callbacks,indent,k,v,node,child,parent)
+-- construct a one-element typed array containing the argument
+-- (useful for "pass-by-reference" or "pass a pointer to" semantics)
+local function   box(what, T) return ffi.new((T or ffi.typeof(what)).."[1]", what) end
+local function unbox(what, T) return T and ffi.cast(T.."*", what)[0] or what[0] end
+
+local function doformat(userdata, callbacks, indent, k, v, node, child, parent)
     -- k         is the one -based index of the ordered fields in 'node'
     -- itemindex is the zero-based index corresponding to k, with 'base' discounted
     -- itemindex indexes into the C structures (node_rec.items[])
-    local itemindex = k - 2
-    -- XXX get rid of special ("base") case
-    if ffi.istype("struct node",node) then itemindex = 0 end
+    --
+    -- in the case of a normal node, k starts at 1, but refers to base, so the
+    -- counting as far as node_rec.items is concerned starts at 2. in the case
+    -- of a `struct node' (the base type), there is no base, so we adjust.
+    local itemindex = ffi.istype("struct node", node) and k - 1 or k - 2
 
     local done
-    local mytag = ffi.tagof(node)
-    if is_anonymous(mytag) and itemindex >= 0 then
+    local tag = ffi.tagof(node)
+    if is_anonymous(tag) and itemindex >= 0 then
         -- TODO breaks on inners (like assignment_inner_)
-        local myrec = rec_from_tag(mytag)
-        local me = libast.node_recs[myrec.type]
-
-        local item = me.items[itemindex]
+        local item = libast.node_recs[ rec_from_tag(tag).type ].items[ itemindex ]
         local dc = decode_node_item(item)
         if not ffi.isnull(dc) then
             local size  = 128
@@ -65,27 +67,30 @@ local function doformat(userdata,callbacks,indent,k,v,node,child,parent)
             local data, pdata
 
             -- XXX get rid of special cases
-            if type(child) == "number" then data = ffi.new("int[1]", child);
-            elseif ffi.istype("uint64_t", child) then pdata = ffi.new("uint64_t[1]", child)
-            elseif ffi.istype("char *", child) then data = child
+            if type(child) == "cdata" then
+                    if ffi.istype("uint64_t", child) then pdata = box(child, "uint64_t")
+                elseif ffi.istype("char *", child)   then  data = child
+                end
+            elseif type(child) == "number" then
+                data = box(child, "int")
             end
 
-            pdata = pdata or ffi.new("void*[1]", data)
+            pdata = pdata or box(data, "void*")
 
             local result = libast.fmt_call(item.meta, dc.type, psize, buf, pdata)
             if result >= 0 then
-                callbacks.walk(userdata,indent,v,ffi.string(buf))
+                callbacks.walk(userdata, indent, v, ffi.string(buf, unbox(psize)))
                 done = true
             end
         end
     end
 
     if not done then
-        callbacks.walk(userdata,indent,v,child)
+        callbacks.walk(userdata, indent, v, child)
     end
 end
 
-function AST.walk(node,userdata,callbacks,parent,indent)
+function AST.walk(node, userdata, callbacks, parent, indent)
     if type(node) ~= "cdata" or not ffi.nsof(node) or ffi.isnull(node) then return nil end
     if not indent then indent = 0 end
     -- TODO don't change incoming callbacks table
@@ -103,16 +108,16 @@ function AST.walk(node,userdata,callbacks,parent,indent)
                 return nil
             end
             local child = node[fields[parent.idx]]
-            callbacks.walk(userdata,indent,fields[parent.idx],child)
-            AST.walk(child,userdata,callbacks,parent,indent+1)
+            callbacks.walk(userdata, indent, fields[parent.idx], child)
+            AST.walk(child, userdata, callbacks, parent, indent+1)
         end
 
     elseif myns == "struct" then
 
-        for k,v in ipairs(fields) do
+        for k, v in ipairs(fields) do
             local child = node[v]
-            doformat(userdata,callbacks,indent,k,v,node,child,parent)
-            AST.walk(child,userdata,callbacks,node,indent+1)
+            doformat(userdata, callbacks, indent, k, v, node, child, parent)
+            AST.walk(child, userdata, callbacks, node, indent + 1)
         end
 
     else
